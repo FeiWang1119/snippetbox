02fb1933c284e0511aee6f4eb5fd1f014b327469
wangfei <wangfei@deepin.org>
2024-10-30 16:55:14 +0800

    Caching templates

--- src/web/handlers.go
+++ src/web/handlers.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"html/template"
+	// "html/template"
 	"net/http"
 	"strconv"
 	"snippetbox/src/pkg/models"
@@ -16,32 +16,48 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	s, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// Use the render() helper.
+	app.render(w, r, "home.page.tmpl", &templateData{Snippets: s})
+
+	// Create an instance of a templateData struct holding the slice of snippets.
+	//
+	// data := &templateData{Snippets: s}
+
 	// Initialize a slice containing the paths to the two files. Note that the home.page.template
 	// file must be the "first" file in the slice.
 	// Include the footer partial in the template files.
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
+	//
+	// files := []string{
+	// 	"./ui/html/home.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
 
 	// Use the template.ParseFiles()) function to read the template file into a template set.
 	// If there is an error, we log the detailed error message and the http.Error() function
 	// to send a generic 500 Internal Server Error response to the user.
 	// Notice that we can pass the slice of file paths as a variadic parameter.
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, err) // Use the serveError() helper.
-		return
-	}
+	//
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err) // Use the serveError() helper.
+	// 	return
+	// }
 
 	// We then use the Execute() method on the template set to write the template content
 	// as the response body. The last parameter to Execute() represents dynamic data that
-	// we want to pass in, which for now we'll leave as nil.
-	err = ts.Execute(w, nil)
-	if err != nil {
-		app.serverError(w, err) // Use the serveError() helper.
-	}
+	// we want to pass in. Pass in the templateData struct when executing the template.
+	//
+	// err = ts.Execute(w, data)
+	// if err != nil {
+	// 	app.serverError(w, err) // Use the serveError() helper.
+	// }
 }
 
 // Change the signature of the showSnippet handler so it is defined as a method against
@@ -65,30 +81,37 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+	// Use the render() helper.
+	app.render(w, r, "show.page.tmpl", &templateData{Snippet: s})
+
 	// Create an instance of a templateData struct holding the snippet data.
-	data := &templateData{Snippet: s}
+	//
+	// data := &templateData{Snippet: s}
 
 	// Initiallize a slice containing the paths to the show.page.tmpl file,
 	// plus the base layout and footer partials that we made earlier.
-	files := []string{
-		"./ui/html/show.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
+	//
+	// files := []string{
+	// 	"./ui/html/show.page.tmpl",
+	// 	"./ui/html/base.layout.tmpl",
+	// 	"./ui/html/footer.partial.tmpl",
+	// }
 
 	// Parse the template files...
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, err)
-		return
-	}
+	//
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
 
 	// And then execute them, Notice how we are passing in the snippets
 	// data (a models.Snippet struct) as the final parameter.
-	err = ts.Execute(w, data)
-	if err != nil {
-		app.serverError(w, err)
-	}
+	//
+	// err = ts.Execute(w, data)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// }
 }
 
 // Change the signature of the createSnippet handler so it is defined as a method against
