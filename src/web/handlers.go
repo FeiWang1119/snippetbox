54f17be2f94ed78b0256c0d51934d4b604b4767d
wangfei <wangfei@deepin.org>
2024-11-19 14:58:40 +0800

    Scaling data validation

--- src/web/handlers.go
+++ src/web/handlers.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	// "html/template"
 	"net/http"
-	"snippetbox/src/pkg/models"
 	"strconv"
-	"strings"
-	"unicode/utf8"
+
+	"snippetbox/src/pkg/forms"
+	"snippetbox/src/pkg/models"
 )
 
 // Change the signature of the home handler so it is defined as a method against
@@ -123,7 +122,10 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "create.page.html", nil)
+	app.render(w, r, "create.page.html", &templateData{
+		// Pass a new empty forms.Form object to the template.
+		Form: forms.New(nil),
+	})
 }
 
 // Change the signature of the createSnippet handler so it is defined as a method against
@@ -148,48 +150,67 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	// Use the r.PostForm.Get() method to retrieve the relevant data fields
 	// from the r.PostForm map.
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	expires := r.PostForm.Get("expires")
+	// title := r.PostForm.Get("title")
+	// content := r.PostForm.Get("content")
+	// expires := r.PostForm.Get("expires")
 
 	// Initialize a map to hold any invalidation errors.
-	errors := make(map[string]string)
+	// errors := make(map[string]string)
 
 	// Check that the title field is not blank and is not more than 100 characters
 	// long. If it fails either of those checks, add a message to the errors
 	// map using the field name as the key.
-	if strings.TrimSpace(title) == "" {
-		errors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		errors["title"] = "This field is too long (maiximum is 100 characters)"
-	}
+	// if strings.TrimSpace(title) == "" {
+	// 	errors["title"] = "This field cannot be blank"
+	// } else if utf8.RuneCountInString(title) > 100 {
+	// 	errors["title"] = "This field is too long (maiximum is 100 characters)"
+	// }
 
 	// Check that the content field is not blank.
-	if strings.TrimSpace(content) == "" {
-		errors["content"] = "This field cannot be blank"
-	}
+	// if strings.TrimSpace(content) == "" {
+	// 	errors["content"] = "This field cannot be blank"
+	// }
 
 	// Check that the expires field is not blank and matches one of the permitted
 	// values ("1", "7", or "365").
-	if strings.TrimSpace(expires) == "" {
-		errors["expires"] = "This field cannot be blank"
-	} else if expires != "365" && expires != "7" && expires != "1" {
-		errors["expires"] = "This field is invalid"
-	}
+	// if strings.TrimSpace(expires) == "" {
+	// 	errors["expires"] = "This field cannot be blank"
+	// } else if expires != "365" && expires != "7" && expires != "1" {
+	// 	errors["expires"] = "This field is invalid"
+	// }
 
 	// If there are any validation errors, re-display the create.page.tmpl
 	// template passing in the validation errors and previously submitted
 	// r.PostForm data.
-	if len(errors) > 0 {
+	// if len(errors) > 0 {
+	// 	app.render(w, r, "create.page.html", &templateData{
+	// 		FormErrors: errors,
+	// 		FormData:   r.PostForm,
+	// 	})
+	// 	return
+	// }
+
+	// Create a new forms.Form struct containing the POSTed data from the
+	// form, then use the validation methods to check the content.
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
+
+	// If the form isn't valid, redisplay the template passing in the
+	// form.Form object as the data.
+	if !form.Valid() {
 		app.render(w, r, "create.page.html", &templateData{
-			FormErrors: errors,
-			FormData:   r.PostForm,
+			Form: form,
 		})
 		return
 	}
 
 	// Create a new snippet record in the database using the form data.
-	id, err := app.snippets.Insert(title, content, expires)
+	// Because the form data (with type url.Values) has been anonymously embedde
+	// in the form.Form struct, we can use the Get() method to retrieve
+	// the validated value for a particular form field.
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 	if err != nil {
 		app.serverError(w, err)
 		return
--- src/web/templates.go
+++ src/web/templates.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 
+	"snippetbox/src/pkg/forms"
 	"snippetbox/src/pkg/models"
 )
 
@@ -15,8 +15,7 @@ import (
 // to it as the build progresses.
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
