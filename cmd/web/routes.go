da8b16a0bf4972571b316f196488cbc5b49f4d49
wangfei <wangfei@deepin.org>
2024-11-27 10:25:39 +0800

    CSRF Protection

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,7 +16,9 @@ func (app *application) routes() http.Handler {
 	// Create a new middleware chain containing the middleware specific to
 	// our dynamic application routes. For now, this chain will only contain
 	// the session middleware but we'll add more to it later.
-	dynamicMiddleware := alice.New(app.session.Enable)
+	// dynamicMiddleware := alice.New(app.session.Enable)
+	// Use the nosurf middleware on all our 'dynamic' routes.
+	dynamicMiddleware := alice.New(app.session.Enable, noSurf)
 
 	// mux := http.NewServeMux()
 	// mux.HandleFunc("/", app.home)
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -15,6 +15,7 @@ import (
 // to it as the build progresses.
 type templateData struct {
 	AuthenticateUser int
+	CSRFToken        string
 	CurrentYear      int
 	Flash            string
 	Form             *forms.Form
